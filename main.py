06aa0f2b8e5e74b888270b38965346d4529172e4
Yogika uraf simran <89250123+yogika@users.noreply.github.com>
2022-01-18 13:35:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+import random
+random=random.randint(1,2)
+if random==1:
+  print("heads")
+else:
+  print("tails")  
+
+
+
+
+
+
+
+
+
